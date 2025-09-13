ffd425c72f06be32cc14ddd8f0ff2d64b39dc9f8
BinaryMusic <dilaweralilinux@gmail.com>
2025-09-13 14:46:38 -0400

    mapping for markdown is added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,12 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+
 map("i", "jk", "<ESC>")
+
+-- Toggle Markdown Preview (GitHub-like view)
+map("n", "<leader>mp", "<cmd>RenderMarkdown toggle<CR>", { desc = "Toggle Markdown Preview" })
+
 
 -- Replace Down arrow functionality
 map({ "n", "i", "v" }, "<A-j>", "<Down>", { desc = "Move down (alt+j instead of down key)" })
